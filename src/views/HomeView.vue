8a8da0082919d8be37211faa39bbca0ac13617a0
carolinelds <carolinelds@gmail.com>
2022-03-18 18:57:17 -0300

    'Clean' App.vue from plugin loaded code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,12 @@
 <template>
-  <hello-world />
+  <div>
+    <h1>Homepage</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'HomeView',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
